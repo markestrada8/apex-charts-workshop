816bdc6ba59d4debb74e5afbd525a4b3ebec0e4d
Jordan Hudgens <jordan@devcamp.com>
2022-05-04 15:12:29 -0700

    Built out starter code

--- src/components/Students.tsx
+++ src/components/Students.tsx
@@ -13,38 +13,6 @@ const data = {
 export default function Students() {
   const [view, setView] = React.useState<"area" | "bar">("bar");
 
-  const series = [
-    {
-      name: "James",
-      data: data.james,
-    },
-    {
-      name: "Gabrielle",
-      data: data.gabrielle,
-    },
-    {
-      name: "Alice",
-      data: data.alice,
-    },
-  ];
-
-  const options = {
-    xaxis: {
-      categories: [
-        "Aug",
-        "Sept",
-        "Oct",
-        "Nov",
-        "Dec",
-        "Jan",
-        "Feb",
-        "Mar",
-        "Apr",
-        "May",
-      ],
-    },
-  };
-
   return (
     <Layout>
       <div style={{ display: "flex", justifyContent: "flex-end" }}>
@@ -69,23 +37,9 @@ export default function Students() {
         </a>
       </div>
 
-      {view === "bar" && (
-        <ReactApexcharts
-          type="bar"
-          height={500}
-          series={series}
-          options={options}
-        />
-      )}
+      {view === "bar" && <h2>TODO: bar chart</h2>}
 
-      {view === "area" && (
-        <ReactApexcharts
-          type="area"
-          height={500}
-          series={series}
-          options={options}
-        />
-      )}
+      {view === "area" && <h2>TODO: area chart</h2>}
     </Layout>
   );
 }
